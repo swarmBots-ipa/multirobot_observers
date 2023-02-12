edc21a860933b538d230ee36ee1634eb5b2a393a
Janavi Ramesh <janavi.ramesh@ipa.fraunhofer.de>
2023-02-12 16:16:44 +0100

    plot for 15 iterations

--- mileage_observer/mileage_observer/graph_plotter.py
+++ mileage_observer/mileage_observer/graph_plotter.py
@@ -3,8 +3,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 class MileageObserverPlotter:
     
     def mil_path_graph(mil_arr,path_arr):
@@ -17,30 +15,31 @@ class MileageObserverPlotter:
             path_arr = trunc(path_arr,decs=2)
             iteration = []
             std =[]
+           
             for j in range(len(path_arr[i])):
                 iteration.append('Iteration'+str(j+1))
                 std.append(0.2)
-
+                
             mileage, mileage_std = mil_arr[i], std
             path_dist, path_dist_std = path_arr[i], std
 
             ind = np.arange(len(mileage))  # the x locations for the groups
             width = 0.35  # the width of the bars
-
+            
             fig, ax = plt.subplots()
             rects1 = ax.bar(ind - width/2, mileage, width, yerr=mileage_std,label='Mileage')
             rects2 = ax.bar(ind + width/2, path_dist, width, yerr=path_dist_std,label='Path distance')
 
             # Add some text for labels, title and custom x-axis tick labels, etc.
             ax.set_ylabel('Distance')
-            ax.set_title('Mileage vs Path distance')
+            ax.set_title('Mileage vs Path distance - Barista : ' + str(i))
             ax.set_xticks(ind)
             
             
 
             ax.set_xticklabels(iteration)
             ax.legend()
-
+            
 
             def autolabel(rects, xpos='center'):
                 ha = {'center': 'center', 'right': 'left', 'left': 'right'}
@@ -60,5 +59,6 @@ class MileageObserverPlotter:
 
             fig.tight_layout()
             plt.savefig('../data/graphs/barista_'+str(i)+'.png')
+        
         plt.show()    
-    
+   
