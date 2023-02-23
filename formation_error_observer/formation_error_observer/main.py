ed3d63aa0bc899878d2f93b5ca2af7f5c3f516e2
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-02-23 17:33:12 +0100

    issue with formation and mileage oberver

--- formation_error_observer/formation_error_observer/main.py
+++ formation_error_observer/formation_error_observer/main.py
@@ -8,23 +8,14 @@ import sys
 pose_nodes = []
 goal_nodes = []
 def main(args=None):
-
+    agents=int(sys.argv[2])
     rclpy.init(args=args)
     try:
-        for i in range(4):
+        for i in range(agents):
             pose_node = PoseSubscriber(sys.argv[1],robot_id = i)
             pose_nodes.append(pose_node)
             goal_node = GoalSubscriber(robot_id = i)
             goal_nodes.append(goal_node)
-
-        #pose_node_0 = PoseSubscriber(sys.argv[1],robot_id = 0)   
-        #pose_node_1 = PoseSubscriber(sys.argv[1],robot_id = 1)
-        #pose_node_2 = PoseSubscriber(sys.argv[1],robot_id = 2)
-        #pose_node_3 = PoseSubscriber(sys.argv[1],robot_id = 3) 
-        #goal_node_0 = GoalSubscriber(robot_id = 0)   
-        #goal_node_1 = GoalSubscriber(robot_id = 1)
-        #goal_node_2 = GoalSubscriber(robot_id = 2)
-        #goal_node_3 = GoalSubscriber(robot_id = 3) 
 
         for pose_node in pose_nodes:
             executor = SingleThreadedExecutor()
@@ -35,18 +26,6 @@ def main(args=None):
             executor = SingleThreadedExecutor()
             executor.add_node(goal_node)
             executor.spin()    
-
-
-        #executor = SingleThreadedExecutor()
-        #executor.add_node(pose_node_0)
-        #executor.add_node(pose_node_1)
-        #executor.add_node(pose_node_2)
-        #executor.add_node(pose_node_3)
-        #executor.add_node(goal_node_0)
-        #executor.add_node(goal_node_1)
-        #executor.add_node(goal_node_2)
-        #executor.add_node(goal_node_3)
-        #executor.spin()
         
     except KeyboardInterrupt:
         #For writing data into json and csv files
--- mileage_observer/mileage_observer/main.py
+++ mileage_observer/mileage_observer/main.py
@@ -2,33 +2,23 @@
 
 import rclpy
 from rclpy.executors import SingleThreadedExecutor
-from multirobot_observers.mileage_observer.mileage_observer.path_observer import PathObserver
+from mileage_observer.path_observer import PathDistance
 from mileage_observer.mileage_observer import MileageObserver
-from multirobot_observers.mileage_observer.mileage_observer.mileage_observer_plotter import MileageObserverPlotter
-
-
-
+from mileage_observer.mileage_observer_plotter import MileageObserverPlotter
 import sys
+
+
 nodes =[]
 mileage_subscribers=[]
 def main(args=None):
-
+    agents=int(sys.argv[2])
     rclpy.init(args=args)
     try:
-        for i in range(4):
-            node = PathObserver(sys.argv[1],robot_id=i)
+        for i in range(agents):
+            node = PathDistance(sys.argv[1],robot_id=i)
             nodes.append(node)
             mileage_subscriber = MileageObserver(sys.argv[1],robot_id=i) 
             mileage_subscribers.append(mileage_subscriber)
-
-        #node_0= PathObserver(sys.argv[1],robot_id=0)
-        #node_1= PathObserver(sys.argv[1],robot_id=1)
-        #node_2= PathObserver(sys.argv[1],robot_id=2)
-        #node_3= PathObserver(sys.argv[1],robot_id=3)
-        #mileage_subscriber_0 = MileageObserver(sys.argv[1],robot_id=0) 
-        #mileage_subscriber_1 = MileageObserver(sys.argv[1],robot_id=1)
-        #mileage_subscriber_2 = MileageObserver(sys.argv[1],robot_id=2)
-        #mileage_subscriber_3 = MileageObserver(sys.argv[1],robot_id=3)
 
         for node in nodes:
             executor = SingleThreadedExecutor()
@@ -38,20 +28,11 @@ def main(args=None):
             executor = SingleThreadedExecutor()    
             executor.add_node(mileage_subscriber)
             executor.spin()
-        #executor = SingleThreadedExecutor()
-        #executor.add_node(node_0)
-        #executor.add_node(node_1)
-        #executor.add_node(node_2)
-        #executor.add_node(node_3)
-        #executor.add_node(mileage_subscriber_0)
-        #executor.add_node(mileage_subscriber_1)
-        #executor.add_node(mileage_subscriber_2)
-        #executor.add_node(mileage_subscriber_3)
-        #executor.spin()
+
         
     except KeyboardInterrupt:
         #For writing data into json and csv files
-        actual_path_length = PathObserver.final_data()
+        actual_path_length = PathDistance.final_data()
         path_travelled =  MileageObserver.final_data()
         print(actual_path_length)
         print(path_travelled)
